d725538d9e9030abff585c9cbf3ecf475f893fe7
Ismail <is.chbiki@gmail.com>
2024-04-07 13:47:23 +0200

    API for local development

--- src/Components/Sports/utils/constants.js
+++ src/Components/Sports/utils/constants.js
@@ -41,6 +41,7 @@ export const services = [
 
 export const sports_url =
   "https://raw.githubusercontent.com/ismailchbiki/data/main/sports.json";
-
-// export const single_sport_url = `https://raw.githubusercontent.com/ismailchbiki/data/main/sports_info.json?id=`; // this is for server-side filtering
 export const single_sport_url = `https://raw.githubusercontent.com/ismailchbiki/data/main/sports_info.json`;
+
+// export const sports_url = "http://127.0.0.1:5000/api/sports";
+// export const single_sport_url = `http://127.0.0.1:5000/api/sports_info`;
